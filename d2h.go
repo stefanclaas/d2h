eff89ffcea0d8a05679fb510cdc4489d6a5c52e3
Stefan Claas <13186616+stefanclaas@users.noreply.github.com>
2023-12-15 15:32:56 +0100

    Add files via upload

---
+++ d2h.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"math/big"
+	"os"
+)
+
+func main() {
+	// Define flags
+	encode := flag.Bool("e", false, "Encode from decimal to hexadecimal")
+	decode := flag.Bool("d", false, "Decode from hexadecimal to decimal")
+	flag.Parse()
+
+	// Check whether exactly one flag is set
+	if (*encode && *decode) || (!*encode && !*decode) {
+		fmt.Println("Please enter exactly one flag: -e for Encode or -d for Decode.")
+		os.Exit(1)
+	}
+
+	// Read input from stdin
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+
+	// Create a new big.Int instance
+	num := new(big.Int)
+
+	if *decode {
+		// Decode: Hexadecimal to decimal
+		num.SetString(input, 16)
+		fmt.Println(num.String())
+	} else if *encode {
+		// Encode: Decimal to hexadecimal
+		num.SetString(input, 10)
+		fmt.Println(num.Text(16))
+	}
+}
+
